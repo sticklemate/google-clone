b2672eb2640fca0a38ccd44da5ecf862a2f60b89
sticklemate <sticklemate@gmail.com>
2021-02-13 22:02:54 +1100

    Update search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,11 @@ import "../styles/Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
-import { useHistory } from 'react-router-dom';
-import { useStateValue } from '../StateProvider'
-import { actionTypes } from '../reducer';
+import { useHistory } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
-function Search({hideButtons = false}) {
+function Search({ hideButtons = false }) {
   //functional component for state management
   const [input, setInput] = useState("");
   const history = useHistory();
@@ -15,14 +15,14 @@ function Search({hideButtons = false}) {
 
   const search = (e) => {
     e.preventDefault();
-    console.log('You typed in ->>'+input);
+    console.log("You typed in ->>" + input);
 
     dispatch({
-        type: actionTypes.SET_SEARCH_TERM,
-        term:input
-    })
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
 
-    history.push('/search');
+    history.push("/search");
 
     //do something with input
   };
@@ -31,26 +31,31 @@ function Search({hideButtons = false}) {
     <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input value={input} onChange={e=>setInput(e.target.value)}></input>
+        <input value={input} onChange={(e) => setInput(e.target.value)}></input>
         <MicIcon />
       </div>
 
       {!hideButtons ? (
-
-      
-      <div className="search__buttons">
-        <Button type='submit' onClick={search} variant="outlined">
-          Google Search
-        </Button>
-        <Button variant="outlined">I'm feeling lucky</Button>
-      </div>
-      ): (
         <div className="search__buttons">
-        <Button className='search_buttonsHidden' type='submit' onClick={search} variant="outlined">
-          Google Search
-        </Button>
-        <Button className='search_buttonsHidden' variant="outlined">I'm feeling lucky</Button>
-      </div>
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm feeling lucky</Button>
+        </div>
+      ) : (
+        <div className="search__buttons">
+          <Button
+            className="search_buttonsHidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button className="search_buttonsHidden" variant="outlined">
+            I'm feeling lucky
+          </Button>
+        </div>
       )}
     </form>
   );
